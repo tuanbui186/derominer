916f8e65cba76c716fbf7f5e81f313fe87b6905f
tuanbui186 <11520451@gm.uit.edu.vn>
2023-04-03 15:32:46 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qy8hvr3akmwpczz0fmms02z6xk7mtx8g5d5a5gygewyykxautraf7qqq9kprw -r nodent2.cpumining.cloud:10100 -r1 community-pools.mysrv.cloud:10300 -r2 dero.rabidmining.com:10300 -p rpc;
+    ./astrominer -w dero1qy8hvr3akmwpczz0fmms02z6xk7mtx8g5d5a5gygewyykxautraf7qqq9kprw -r community-pools.mysrv.cloud:10300 -r1 dero.rabidmining.com:10300 -p rpc;
     sleep 5;
 done
